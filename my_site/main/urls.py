db8691d96b852f66013c5e43215c3b7a726b9b5e
Nale <n_wanyama@yahoo.com>
2023-03-03 15:25:40 +0300

    Using Bootstrap to customize website

--- my_site/main/urls.py
+++ my_site/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.base, name="base"),
     path("<int:id>", views.index, name="index"),
+    path("home/", views.home, name="home"),
     path("create/", views.create, name="create")
 ]
--- my_site/main/views.py
+++ my_site/main/views.py
@@ -1,20 +1,58 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 
 # Create your views here.
 def base(response):
-     return render(response, 'main/base.html', {"name": "test"})
+     return render(response, 'main/base.html', {"name": "base"})
+
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+
+        if response.POST.get("save"):
+
+            for item in ls.item_set.all():
+
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("newListItem")
+
+            if len(txt) > 2:
+                ls.item_set.create(name=txt, complete=False)
+            else:
+                print("Invalid. Enter 3 characters or more.")
+
+
     return render(response, 'main/list.html', {"ls": ls})
 
-def home(response, name):
-    my_dict = {"name": name}
-    return render(response, 'main/home.html', my_dict)
+
+def home(response):
+    #my_dict = {"name": name}
+    return render(response, 'main/home.html', {})
+
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
+
     return render(response, 'main/create.html', {"form": form})
